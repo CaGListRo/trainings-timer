f8229187ab95cbe99763a8746af02316673ce11e
CaGListRo <paco.nyns@gmail.com>
2025-05-26 20:03:17 +0200

    WIP: Added Buttons for pre timer setting. Added the text for the pre timer and added an overtime text which is displayed when the set time is up.

--- trainings_timer.py
+++ trainings_timer.py
@@ -17,17 +17,31 @@ class TrainingsTimer:
         self.timer_running: bool = False
         self.count_down: bool = True
         self.time: float = 0.0
+        self.pre_timer: float = 0.0
         self.fps: int = 0
         self.create_buttons()
 
+        # fonts
+        self.small_font: pg.font.Font = pg.font.SysFont("comic sans", 23)
         self.time_font: pg.font.Font = pg.font.SysFont("comic sans", 55, bold=True)
-        self.plus: pg.Surface = self.time_font.render("+", True, "white")
+
+        # pre-render
+        self.plus_sign: pg.Surface = self.time_font.render("+", True, "white")
+        self.pre_timer_text: pg.Surface = self.small_font.render("pre timer in s", True, "white")
+        self.overtime_text: pg.Surface = self.time_font.render("OVERTIME", True, "white")
+        overtime_x: int = round(self.WIDTH / 2 - self.overtime_text.get_width() / 2)
+        overtime_y: int = round(self.HEIGHT / 3 - self.overtime_text.get_height() / 2)
+        self.overtime_pos: tuple[int, int] = (overtime_x, overtime_y)
 
     def check_buttons(self) -> None:
         """ Checks if a button is clicked. """
-        for idx, btn in enumerate(self.button_list):
+        for idx, btn in enumerate(self.time_button_list):
             if btn.check_clicked() and not self.timer_running:
                 self.time = float((idx + 1) * 2 * 60)
+                self.count_down = True
+        for idx, btn in enumerate(self.pre_timer_button_list):
+            if btn.check_clicked() and not self.timer_running:
+                self.pre_timer = float(idx * 10)
                 self.count_down = True
         if self.start_button.check_clicked():
             self.timer_running = True
@@ -37,7 +51,8 @@ class TrainingsTimer:
     def create_buttons(self) -> None:
         """ Creates all the buttons. """
         print("Create btn")
-        self.button_list: List[Btn] = [Btn(pos=(5 + i * 55, 5), size=(50, 50), text=(str((i + 1) * 2)), color="white") for i in range(10)]
+        self.time_button_list: List[Btn] = [Btn(pos=(5 + i * 55, 10), size=(50, 50), text=(str((i + 1) * 2)), color="white") for i in range(10)]
+        self.pre_timer_button_list: List[Btn] = [Btn(pos=(5, 170 + i * 55), size=(50, 50), text=str(i * 10), color="white") for i in range(4)]
         self.start_button: Btn = Btn(pos=(175, 455), size=(100, 50), text="START", color="green")
         self.stop_button: Btn = Btn(pos=(280, 455), size=(100, 50), text="STOP", color="red")
 
@@ -61,8 +76,13 @@ class TrainingsTimer:
         """ Draws all the elements on the screen. """
         pg.display.set_caption(f"Trainings Timer          FPS:{self.fps}")
         self.screen.fill("black")
-        for btn in self.button_list:
-            btn.render(self.screen)
+        if not self.timer_running:
+            for btn in self.time_button_list:
+                btn.render(self.screen)
+            for btn in self.pre_timer_button_list:
+                btn.render(self.screen)
+            rotated_pre_timer_text: pg.Surface = pg.transform.rotate(self.pre_timer_text, 90)
+            self.screen.blit(rotated_pre_timer_text, (60, 197))
         self.start_button.render(self.screen)
         self.stop_button.render(self.screen)
         minute: int = int(self.time / 60)
@@ -74,7 +94,8 @@ class TrainingsTimer:
         time_y_pos: int = round(self.HEIGHT / 2 - time_to_blit.get_height() / 2)
         self.screen.blit(time_to_blit, (time_x_pos, time_y_pos))
         if not self.count_down:
-            self.screen.blit(self.plus, (time_x_pos - 50, time_y_pos))
+            self.screen.blit(self.overtime_text, self.overtime_pos)
+            self.screen.blit(self.plus_sign, (time_x_pos - 50, time_y_pos))
 
         pg.display.update()
 
